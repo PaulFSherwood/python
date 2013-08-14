771c5d7929f7a31854adc811117a2d93eda25079
paul <paulfsherwood@gmail.com>
2013-08-14 08:37:29 -0600

    from tutorial
    using text fields input fields and buttons.
    some formatting text.

---
+++ gui/test05.py
@@ -0,0 +1,57 @@
+from tkinter import *
+from tkinter import ttk
+
+def calculate(*args):
+    try:
+        value = float(feet.get())
+        meters.set((0.3048 * value * 10000.0 + 0.5)/10000.0)
+    except ValueError:
+        pass
+    
+root = Tk()
+root.title("Feet to Meters")
+
+mainframe = ttk.Frame(root, padding="3 3 12 12")
+mainframe.grid(column=0, row=0)#, sticky=(N, W, E, S))
+mainframe.columnconfigure(0, weight=1)
+mainframe.rowconfigure(0, weight=1)
+
+feet = StringVar()
+meters = StringVar()
+
+# text entry field
+# __________________________________
+# |          |TXT ENTY  |          |
+# |          |          |          |
+# |          |          |          |
+# ----------------------------------
+feet_entry = ttk.Entry(mainframe, width=7, textvariable=feet)
+feet_entry.grid(column=2, row=1, sticky=(W, E))
+
+# changing fields
+# __________________________________
+# |          |          |          |
+# |          |CHGIN LBL |          |
+# |          |          |CALC TXT  |
+# ----------------------------------
+ttk.Label(mainframe, textvariable=meters).grid(column=2, row=2, sticky=(W, E))
+ttk.Button(mainframe, text="Calculate", command=calculate).grid(column=3, row=3, sticky=W)
+
+# text only section
+# __________________________________
+# |          |          |feet      |
+# |is eqiv to|          |          |
+# |          |          |meters    |
+# ----------------------------------
+ttk.Label(mainframe, text="feet").grid(column=3, row=1, sticky=W)
+ttk.Label(mainframe, text="is equivalent to").grid(column=1, row=2, sticky=E)
+ttk.Label(mainframe, text="meters").grid(column=3, row=2, sticky=W)
+
+# wrap everything in a padding
+# so fields arent pressed against each other
+for child in mainframe.winfo_children(): child.grid_configure(padx=5, pady=5)
+
+feet_entry.focus()
+root.bind('<Return>', calculate)
+
+root.mainloop()
