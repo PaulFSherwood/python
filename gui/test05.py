fe7eb5bca8af72b8de3e11b6ae5ce72e4b710e44
paul <paulfsherwood@gmail.com>
2013-08-14 08:58:53 -0600

    comment update

--- gui/test05.py
+++ gui/test05.py
@@ -13,6 +13,7 @@ root.title("Feet to Meters")
 
 mainframe = ttk.Frame(root, padding="3 3 12 12")
 mainframe.grid(column=0, row=0)#, sticky=(N, W, E, S))
+# when expanding window how fast do columns/rows grow
 mainframe.columnconfigure(0, weight=1)
 mainframe.rowconfigure(0, weight=1)
 
