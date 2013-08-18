bfed2bedfe11dbe774b7c654b88f0b32c4809804
paul <paulfsherwood@gmail.com>
2013-08-18 11:25:25 -0600

    adding in file open and color chooser

---
+++ gui/test07.py
@@ -0,0 +1,73 @@
+import sys
+from tkinter import *
+from tkinter import messagebox
+from tkinter import colorchooser
+from tkinter import filedialog
+
+def mhello():
+    mtext=ment.get()
+    mlabel2 = Label(mGui,text=mtext).pack()
+    return
+
+def mNew():
+    mlabel3 = Label(mGui,text="You Clicked New").pack()
+    return
+
+def mAbout():
+	# messagebox.showinfo(title="About", message = "This is my About Box")
+	# messagebox.showwarning(title="About", message = "This is my About Box")
+	messagebox.showerror(title="About", message = "This is my About Box")
+	return
+
+def mQuit():
+    mExit = messagebox.askyesno(title="Quit", message="Do you want to Quit")
+    if mExit > 0:
+        mGui.destroy()
+        return
+
+def mColor():
+    mycolor = colorchooser.askcolor()
+    mlabel4 = Label(mGui,text=mycolor).pack()
+    return
+
+def mOpen():
+    myopen = filedialog.askopenfile()
+    return
+
+mGui = Tk()
+ment = StringVar()
+
+mGui.geometry('450x450+500+300')
+mGui.title('My Youtube Tkinter')
+
+mlabel = Label(mGui, text='My Label').pack()
+mbutton = Button(mGui, text='OK', command=mhello, fg='red', bg='blue').pack()
+mEntry = Entry(mGui, textvariable=ment).pack()
+
+# Menu Construction
+# menubar object
+menubar=Menu(mGui)
+
+# our list name
+filemenu = Menu(menubar, tearoff=0)
+# making a list of what will be in the drop down
+filemenu.add_command(label = "New",   command = mNew)
+filemenu.add_command(label = "Open",  command = mOpen)
+filemenu.add_command(label = "Save As..")
+filemenu.add_command(label = "Color", command = mColor)
+filemenu.add_command(label = "Exit",  command = mQuit)
+# adding our list to the file button
+menubar.add_cascade(label="File", menu=filemenu)
+
+# Help Menu
+helpmenu = Menu(menubar, tearoff = 0)
+helpmenu.add_command(label = "Help Docs")
+helpmenu.add_command(label = "About", command = mAbout)
+# adding our list to the file button
+menubar.add_cascade(label  = "Help",  menu    = helpmenu)
+
+# add file menu to the screen
+mGui.config(menu=menubar)
+
+
+mGui.mainloop()
