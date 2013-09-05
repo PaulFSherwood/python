135c9301d533642180d54752fed1c60fddf41636
paul <paulfsherwood@gmail.com>
2013-09-04 20:48:25 -0600

    bg color change

--- gui/canvas.py
+++ gui/canvas.py
@@ -6,6 +6,6 @@ mGui.geometry('450x600+300+100')
 mGui.title('Canvas test')
 
 # Canvas
-canvas_1 = Canvas(mGui, height = 300, width = 300).pack()
+canvas_1 = Canvas(mGui, height = 300, width = 300, bg='#123456').pack()
 
 mGui.mainloop()
