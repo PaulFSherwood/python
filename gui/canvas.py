301c08b1a9b5698a09bb6e3436b084ff4df38513
paul <paulfsherwood@gmail.com>
2013-09-05 20:14:44 -0600

    draw lines

--- gui/canvas.py
+++ gui/canvas.py
@@ -6,6 +6,10 @@ mGui.geometry('450x600+300+100')
 mGui.title('Canvas test')
 
 # Canvas
-canvas_1 = Canvas(mGui, height = 300, width = 300, bg='#123456').pack()
+canvas_1 = Canvas(mGui, height = 300, width = 300, bg='#123456')
+
+canvas_1.create_line(0,0,300,300)
+
+canvas_1.pack()
 
 mGui.mainloop()
