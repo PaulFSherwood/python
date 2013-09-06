5a957dc93443e5f04f49b21d40c12bb869ec37e2
paul <paulfsherwood@gmail.com>
2013-09-05 20:21:11 -0600

    added oval and rectangle

--- gui/canvas.py
+++ gui/canvas.py
@@ -10,6 +10,10 @@ canvas_1 = Canvas(mGui, height = 300, width = 300, bg='#123456')
 
 canvas_1.create_line(0,0,300,300)
 
+canvas_1.create_oval(100,100,200,200)
+
+canvas_1.create_rectangle(25,24,50,50)
+
 canvas_1.pack()
 
 mGui.mainloop()
