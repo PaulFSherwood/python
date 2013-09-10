575c02ad7a734a890c2272ca8457740104cf92c4
paul <paulfsherwood@gmail.com>
2013-09-09 19:16:22 -0600

    Connect to Flight Gear with perl and send it information.

--- http_test.py
+++ http_test.py
@@ -1,19 +1,6 @@
-import socket
- 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("127.0.0.1", 5555))
+import urllib.request
 
-# # Echo client program
-# import socket
-# 
-# HOST = '127.0.0.1'    # The remote host
-# PORT = 5555              # The same port as used by the server
-# s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# s.connect((HOST, PORT))
-# s.send('Hello, world')
-# data = s.recv(1024)
-# s.close()
-# print 'Received', repr(data)
+# access the flight gear webserver and send it the value we want.
+urllib.request.urlopen("http://127.0.0.1:5555/instrumentation/nd/range?value=10&submit=update").read()
 
-#socket.connect('http://127.0.0.1/instrumentation/nd/range?value=10&submit=update', '5555')
 
