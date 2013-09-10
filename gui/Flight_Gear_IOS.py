2a46f66c6edad08a64619cf05f433e9973f39b3b
paul <paulfsherwood@gmail.com>
2013-09-09 20:37:05 -0600

    Flight Gear IOS update
    trying to fix up the url's used. cant seem to use a variable in the url.

---
+++ gui/Flight_Gear_IOS.py
@@ -0,0 +1,34 @@
+from tkinter import *
+from tkinter import ttk
+import urllib.request
+
+def setND1():
+   # access the flight gear webserver and send it the value we want.
+   urllib.request.urlopen("http://127.0.0.1:5555/instrumentation/nd/range?value=10&submit=update").read()
+   pass
+
+def setND2():
+   # access the flight gear webserver and send it the value we want.
+   urllib.request.urlopen("http://127.0.0.1:5555/instrumentation/nd/range?value=100&submit=update").read()
+   pass
+
+root = Tk()
+root.title("Flight Gear IOS")
+
+mainframe = ttk.Frame(root, padding="3 3 12 12")
+mainframe.grid(column=0, row=0)
+
+# resize stuff
+mainframe.columnconfigure(0, weight=1)
+mainframe.rowconfigure(0, weight=1)
+
+# buttons
+ttk.Button(mainframe, text="ND 10", command=setND1).grid(column=1, row=2, sticky=W)
+ttk.Button(mainframe, text="ND 100", command=setND2).grid(column=1, row=1, sticky=W)
+
+
+# wrap everything in a padding
+# so fields arent pressed against each other
+for child in mainframe.winfo_children(): child.grid_configure(padx=5, pady=5)
+
+root.mainloop()
