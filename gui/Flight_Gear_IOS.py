62a4b5410fdebf7e51cf0643c1fccce79f2ac955
paul <paulfsherwood@gmail.com>
2013-09-09 20:47:40 -0600

    broke, trying to work out using variables.

--- gui/Flight_Gear_IOS.py
+++ gui/Flight_Gear_IOS.py
@@ -2,9 +2,14 @@ from tkinter import *
 from tkinter import ttk
 import urllib.request
 
-def setND1():
+urlUpper = 'http://127.0.0.1:5555/instrumentation/nd/range?value='
+urlLower = '&submit=update'
+
+
+def setND1(Number):
    # access the flight gear webserver and send it the value we want.
-   urllib.request.urlopen("http://127.0.0.1:5555/instrumentation/nd/range?value=10&submit=update").read()
+   full_url = urlUpper + Number + urlLower
+   urllib.request.urlopen(full_url).read()
    pass
 
 def setND2():
@@ -23,8 +28,8 @@ mainframe.columnconfigure(0, weight=1)
 mainframe.rowconfigure(0, weight=1)
 
 # buttons
-ttk.Button(mainframe, text="ND 10", command=setND1).grid(column=1, row=2, sticky=W)
-ttk.Button(mainframe, text="ND 100", command=setND2).grid(column=1, row=1, sticky=W)
+ttk.Button(mainframe, text="ND 10", command=setND1(10)).grid(column=1, row=2, sticky=W)
+ttk.Button(mainframe, text="ND 100", command=setND1(100)).grid(column=1, row=1, sticky=W)
 
 
 # wrap everything in a padding
