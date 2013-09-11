f12202bf03adf6cc37b6c98d6adecac5c3a80f1e
paul <paulfsherwood@gmail.com>
2013-09-10 22:26:17 -0600

    crappy update. cant get the slider to auto update.

--- gui/Flight_Gear_IOS.py
+++ gui/Flight_Gear_IOS.py
@@ -3,9 +3,10 @@ from tkinter import ttk
 import sys
 import urllib.request
 
-def setND(adVal):
+def setND():
    # geting the variable
    currentAddress = "http://127.0.0.1:5555/instrumentation/nd/range?value={0}&submit=update".format(str(adVal.get()))
+   # currentAddress = "http://127.0.0.1:5555/instrumentation/nd/range?value=10&submit=update")
    # access the flight gear webserver and send it the value we want.
    urllib.request.urlopen(currentAddress).read()
    pass
@@ -32,14 +33,14 @@ mainframe.columnconfigure(0, weight=1)
 mainframe.rowconfigure(0, weight=1)
 
 # buttons
-ttk.Button(mainframe, text="ND 10", command=setND1).grid(column=1, row=2, sticky=W)
+ttk.Button(mainframe, text="ND 10", command=setND).grid(column=1, row=2, sticky=W)
 ttk.Button(mainframe, text="ND 100", command=setND2).grid(column=1, row=1, sticky=W)
 
 # slider
 adVal = StringVar()
-Slider_1 = Scale(root, variable = adVal, command=setND(adVal), orient=HORIZONTAL, length=300, width=20, sliderlength=10, from_=0,to=50, tickinterval=5).grid(column=1, row=2, sticky=W)
+Slider_1 = Scale(root, variable=adVal, orient=HORIZONTAL, length=300, width=20, sliderlength=10, from_=0,to=50, tickinterval=5).grid(column=1, row=2, sticky=W)
 
-# Slider_1.on_changed(setND)
+# Slider_1.on_changed()
 
 # wrap everything in a padding
 # so fields arent pressed against each other
