cf5f8f49ed2d3b50b0a593220fdb0a39c9ca434b
paul <paulfsherwood@gmail.com>
2018-02-21 14:21:19 -0600

    Update Flight_Gear_IOS.py
    cleaned up the getRollDeg funcition.  was able to parse based on what is in the value tag.
    would like to add a 3 mile final have some issues that i am off but barely.

--- gui/Flight_Gear_IOS.py
+++ gui/Flight_Gear_IOS.py
@@ -6,107 +6,125 @@ import itertools
 import math
 import sys
 
+IPAddress = "http://127.0.0.1"
+port = "5500"
+
+webAddress = IPAddress + ":" + port
+
 
 def setND(val=None):
-   # geting the variable
-   currentAddress = "http://127.0.0.1:5500/props/instrumentation/nd?submit=set&range%5B0%5D=" + format(str(val))
+    # geting the variable
+    # currentAddress = "http://127.0.0.1:5500/props/instrumentation/nd?submit=set&range%5B0%5D=" + format(str(val))
+    currentAddress = webAddress + "/props/instrumentation/nd?submit=set&range%5B0%5D=" + format(str(val))
 
-   ## http://127.0.0.1:5500/props/orientation
-   # access the flight gear webserver and send it the value we want.
-   urllib.request.urlopen(currentAddress)
-   pass
+    ## http://127.0.0.1:5500/props/orientation
+    # access the flight gear webserver and send it the value we want.
+    urllib.request.urlopen(currentAddress)
+    pass
+
+
 # SC840 Navigation Control Panel (is that right?)
 def setOFF1():
-   # access the flight gear webserver and send it the value we want.
-   urllib.request.urlopen("http://127.0.0.1:5500/instrumentation/primus2000/sc840/nav1ptr?value=0&submit=update").read()
-   adVal = 10
-   pass
+    # access the flight gear webserver and send it the value we want.
+    urllib.request.urlopen(
+        webAddress + "/props/instrumentation/primus2000/sc840/nav1ptr?value=0&submit=update").read()
+    adVal = 10
+    pass
+
 
 def setVOR1():
-   # access the flight gear webserver and send it the value we want.
-   urllib.request.urlopen("http://127.0.0.1:5500/props/instrumentation/primus2000/sc840/nav1ptr?value=1&submit=update").read()
-   adVal = 100
-   pass
+    # access the flight gear webserver and send it the value we want.
+    urllib.request.urlopen(
+        webAddress + "/props/instrumentation/primus2000/sc840/nav1ptr?value=1&submit=update").read()
+    adVal = 100
+    pass
+
 
 def setADF1():
-   # access the flight gear webserver and send it the value we want.
-   urllib.request.urlopen("http://127.0.0.1:5500/props/instrumentation/primus2000/sc840/nav1ptr?value=2&submit=update").read()
-   adVal = 100
-   pass
+    # access the flight gear webserver and send it the value we want.
+    urllib.request.urlopen(
+        webAddress + "/props/instrumentation/primus2000/sc840/nav1ptr?value=2&submit=update").read()
+    adVal = 100
+    pass
+
 
 def setFMS1():
-   # access the flight gear webserver and send it the value we want.
-   urllib.request.urlopen("http://127.0.0.1:5500/props/instrumentation/primus2000/sc840/nav1ptr?value=3&submit=update").read()
-   adVal = 100
-   pass
+    # access the flight gear webserver and send it the value we want.
+    urllib.request.urlopen(
+        webAddress + "/props/instrumentation/primus2000/sc840/nav1ptr?value=3&submit=update").read()
+    adVal = 100
+    pass
+
 
 def setAP1():
-    urllib.request.urlopen("http://127.0.0.1:5500/props/autopilot/locks/AP-status?value=AP1&submit=update").read()
+    urllib.request.urlopen(webAddress + "/props/autopilot/locks/AP-status?value=AP1&submit=update").read()
     pass
 
+
 def setAP2():
-    urllib.request.urlopen("http://127.0.0.1:5500/props/autopilot/locks/AP-status?value=&submit=update").read()
+    urllib.request.urlopen(webAddress + "/props/autopilot/locks/AP-status?value=&submit=update").read()
     pass
+
+
 def setCRS1():
     crsVal = course.get()
-    one = "http://127.0.0.1:5500/props/instrumentation/nav/radials/selected-deg?value="
+    one = webAddress + "/props/instrumentation/nav/radials/selected-deg?value="
     two = "&submit=update"
-    
+
     combined = one + crsVal + two
-    urllib.request.urlopen( combined ).read()
+    urllib.request.urlopen(combined).read()
     pass
+
+
 def setHDG1():
     hdgVal = heading.get()
-    one = "http://127.0.0.1:5500/props/autopilot/settings/heading-bug-deg?value="
+    one = webAddress + "/props/autopilot/settings/heading-bug-deg?value="
     two = "&submit=update"
-    
+
     combined = one + hdgVal + two
-    urllib.request.urlopen( combined ).read()
+    urllib.request.urlopen(combined).read()
     pass
+
+
 def setROLL():
     rolVal = roll.get()
-    one = "http://127.0.0.1:5500/props/autopilot/settings/target-roll-deg?value="
+    one = webAddress + "/props/autopilot/settings/target-roll-deg?value="
     two = "&submit=update"
 
     combined = one + rolVal + two
-    urllib.request.urlopen( combined ).read()
+    urllib.request.urlopen(combined).read()
 
     currentRoll = getRollDeg()
     # print(getRollDeg())
     l1.configure(text=currentRoll)
 
-
     ### /autopilot/locks/heading = ROLL
     ### /autopilot/settings/target-roll-deg = -blag  to 0 to +blarg
 
+
 def getRollDeg():
-    
-    url = "http://127.0.0.1:5500/props/orientation/roll-deg?value="
-    
-    usock = urllib.request.urlopen(url)  # open html
-    data = usock.read()                 # stuff html into variable
-    usock.close()                       # cleanup
-    soup = BeautifulSoup(data)          # make a BS object (i think/pretysure)
-    
-    value = 0
-    number = 0
-    # trying to hold onto the last number (should be the variable we need)
-    # would rather have it pull from the html using the tags, but can't find
-    # the right wording to use in BeautifulSoup4
-    for link in soup.find_all("input"):
-        value = link.get("value")
-        # print(value)
-    
-    # this thing realy doesn't want to be a float do some conversion
-    number = float(value)
-    isinstance(number, float)
+    # current address
+    url = webAddress + "/props/orientation/roll-deg"
+    site = urllib.request.urlopen(url).read()  # read site into variable
+    soup = BeautifulSoup(site, "html.parser") # parse the site for reading
+    number = float(soup.find(attrs={"name": "roll-deg[0]"})['value']) # pull value and convert to float
+
     # shorten to one decimal place
-    number = math.floor(number*10)/10
-    
-    #print(number)
+    number = math.floor(number * 10) / 10
+
+    # print(number)
     return number
 
 
+# def set3nmFinal():
+    ### THROTTLES
+    # http://127.0.0.1:5500/props/controls/engines/engine[1]?submit=set&throttle%5B0%5D=0.5
+    # http://127.0.0.1:5500/props/controls/engines/engine?submit=set&throttle%5B0%5D=0.5
+
+    ### ALTITUDE
+    # http://127.0.0.1:5500/props/position?submit=set&altitude-ft%5B0%5D=2500
+
+    # some stuff from latlongTest.py
 
 
 root = Tk()
@@ -145,10 +163,10 @@ ttk.Button(mainframe, text="AP1/ON", command=setAP1).grid(column=2, row=1, stick
 ttk.Button(mainframe, text="AP1/OFF", command=setAP2).grid(column=2, row=2, sticky=W)
 
 # slider
-Slider_1 = Scale(root, command=setND, orient=HORIZONTAL, length=200, width=20, sliderlength=10, from_=0,to=100)
+Slider_1 = Scale(root, command=setND, orient=HORIZONTAL, length=200, width=20, sliderlength=10, from_=0, to=100)
 Slider_1.set(20)
 # this puts numbers on the bottom , tickinterval=5)
-# not sure why grid isnt working correctly 
+# not sure why grid isnt working correctly
 Slider_1.grid(column=0, row=5, sticky=W)
 
 # entry
