cb329397bf93441f79a6603d071f4d9c56bb2ce4
Paul Sherwood <paulfsherwood@gmail.com>
2022-10-11 00:53:05 -0600

    pandas and csv files

--- pokemon/pandas-pokemon.py
+++ pokemon/pandas-pokemon.py
@@ -6,20 +6,27 @@ df = pd.read_csv('pokemon_data.csv')
 # print(df.tail(2))
 
 ## read headers
-print(df.columns)
+# print(df.columns)
 # print(df['Name'].head(3))
 # print(df['Generation'].head(3))
 
 # print multiple colums
-print(df[['Name', 'HP', 'Defense', 'Generation']].head(3))
+# print(df['Sp. Def'])
+# print(df[['Name', 'HP', 'Defense', 'Generation']].head(3))
 # print(df.Attack.head(3))
 
 # print out a row
-print(df.iloc[4:6])
-print(df)
-for index, row in df.iterrows():
-    # if c['Name'] == 'Venusaur':
-    print(index, row) ## row['Name']
+# print(df.iloc[4:6])
+# print(df)
+# for index, row in df.iterrows():
+# print(df.loc[df['Type 1'] == 'Grass'])  # don't need a loop with this
+# print(df.loc[df['Sp. Def'] == 65])  # don't need a loop with this
+        # print(index, row) ## row['Name']
 
 # get a specific location (R, C)
-print(df.iloc[2, 1])
+# print(df.iloc[2, 1])
+
+## sorting / describing data
+# df.describe()
+
+print(df.sort_values(['Type 1', 'HP'], ascending=[True, False]))
